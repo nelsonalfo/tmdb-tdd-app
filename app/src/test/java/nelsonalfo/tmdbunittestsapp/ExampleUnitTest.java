8adef7ff6f7f0d6ab9c26b7b34bcd1345240a65b
Nelson Ramirez <nelsonalfo@gmail.com>
2017-12-26 19:05:03 -0400

    agregada la libreria Truth para manejar assertions

--- app/src/test/java/nelsonalfo/tmdbunittestsapp/ExampleUnitTest.java
+++ app/src/test/java/nelsonalfo/tmdbunittestsapp/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package nelsonalfo.tmdbunittestsapp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static com.google.common.truth.Truth.assertThat;
 
 
 /**
@@ -13,6 +13,6 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertThat(2+2).isEqualTo(4);
     }
 }
