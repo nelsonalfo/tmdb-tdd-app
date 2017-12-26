671455b4ab3acaa6fc28212f90295c1ebdc6319d
Nelson Ramirez <nelsonalfo@gmail.com>
2017-12-26 14:33:02 -0400

    creada pantalla de Movie List

--- app/src/main/java/nelsonalfo/tmdbunittetsapp/screens/list/MovieListActivity.java
+++ app/src/main/java/nelsonalfo/tmdbunittetsapp/screens/list/MovieListActivity.java
@@ -1,14 +1,16 @@
-package nelsonalfo.themoviedbunittetsapplication;
+package nelsonalfo.tmdbunittetsapp.screens.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import nelsonalfo.tmdbunittetsapp.R;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MovieListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_list);
     }
 }
