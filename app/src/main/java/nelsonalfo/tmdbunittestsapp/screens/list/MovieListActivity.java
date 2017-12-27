bc8a2a38cfa147dff060a06e3732f85a38f15235
Nelson Ramirez <nelsonalfo@gmail.com>
2017-12-27 11:27:03 -0400

    agregando butterKnife a la app

--- app/src/main/java/nelsonalfo/tmdbunittestsapp/screens/list/MovieListActivity.java
+++ app/src/main/java/nelsonalfo/tmdbunittestsapp/screens/list/MovieListActivity.java
@@ -2,15 +2,23 @@ package nelsonalfo.tmdbunittestsapp.screens.list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import nelsonalfo.tmdbunittestsapp.R;
 
 
 public class MovieListActivity extends AppCompatActivity {
+    @BindView(R.id.movie_list)
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        ButterKnife.bind(this);
     }
 }
